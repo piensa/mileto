8030781a920d5aef5eae7e7ce8e132aef9645c86
Ariel <ingenieroariel@gmail.com>
2019-08-09 18:30:17 -0500

    Added builder for admin0

--- default.nix
+++ default.nix
@@ -1,13 +1,19 @@
 let 
     pkgs = import (builtins.fetchTarball https://github.com/NixOS/nixpkgs/archive/release-19.03.tar.gz) {};
     piensa = import (builtins.fetchTarball https://github.com/piensa/nur-packages/archive/dce6ae5.tar.gz) {};
+    ne = https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/;
 in let
-    countries = pkgs.fetchzip { 
-                   url = https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/ne_10m_admin_0_countries.zip;
+    admin0 = pkgs.fetchzip { 
+                   url = ne + "ne_10m_admin_0_countries.zip?version=4.1.0";
                    sha256 = "17xzdnbd083x1gzw4xmmdhpar004hln080dxhsfpf2wiic9siy1y";
                    stripRoot = false;
                 };
-    
+    admin1 = pkgs.fetchzip { 
+                   url = ne + "ne_10m_admin_1_states_provinces.zip";
+                   sha256 = "0hzdqx1lzckflqizacs7s8mhsszdyivp1a2cmkaj0b380mhw7xlj";
+                   stripRoot = false;
+                };
+    d = derivation { name = "foo"; builder = "${pkgs.gdal}/bin/ogr2ogr"; args = [ "-f -f GeoJSON " ]; src = admin0; system = builtins.currentSystem; };
 
 in pkgs.stdenv.mkDerivation rec {
   name = "mileto";
@@ -15,10 +21,16 @@ in pkgs.stdenv.mkDerivation rec {
     piensa.tippecanoe
     pkgs.caddy
     pkgs.bash
-    countries
+    pkgs.gdal
+    admin0
+    admin1
+    d
   ];
 
   shellHooks = ''
+    export ADMIN0=${admin0}
+    export ADMIN1=${admin0}
+    ogr2ogr -f GeoJSON ne_10m_admin_1_states_provinces.geojson ne_10m_admin_1_states_provinces.shp
   '';
 }
 
