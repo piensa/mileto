022c029a57b79671b2df9d12f22fd9a0bb62e769
Your Name <you@example.com>
2019-08-11 16:50:05 -0500

    Added mileto-generate mileto-delete and mileto-serve options

--- default.nix
+++ default.nix
@@ -4,7 +4,7 @@ let
     ne = https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/10m/cultural/;
 in let
     admin0 = pkgs.fetchzip { 
-                   url = ne + "ne_10m_admin_0_countries.zip?version=4.1.0";
+                   url = ne + "ne_10m_admin_0_countries.zip";
                    sha256 = "17xzdnbd083x1gzw4xmmdhpar004hln080dxhsfpf2wiic9siy1y";
                    stripRoot = false;
                 };
@@ -13,26 +13,38 @@ in let
                    sha256 = "0hzdqx1lzckflqizacs7s8mhsszdyivp1a2cmkaj0b380mhw7xlj";
                    stripRoot = false;
                 };
+    gdal = pkgs.gdal;
+    tippecanoe = piensa.tippecanoe;
+    caddy = pkgs.caddy;
 in let
-    build-tiles = pkgs.writeShellScriptBin "build-tiles" ''
+    mileto-generate = pkgs.writeShellScriptBin "mileto-generate" ''
        mkdir -p data
-       ogr2ogr -f GeoJSON data/ne_10m_admin_0_countries.geojson ${admin0}/ne_10m_admin_0_countries.shp;
-       ogr2ogr -f GeoJSON data/ne_10m_admin_1_states_provinces.geojson ${admin1}/ne_10m_admin_1_states_provinces.shp;
-       tippecanoe -z3 -o data/countries-z3.mbtiles --coalesce-densest-as-needed data/ne_10m_admin_0_countries.geojson
-       tippecanoe -zg -Z4 -o data/states-Z4.mbtiles --coalesce-densest-as-needed --extend-zooms-if-still-dropping data/ne_10m_admin_1_states_provinces.geojson
-       tile-join --output-to-directory=tiles data/countries-z3.mbtiles data/states-Z4.mbtiles
+       ${gdal}/bin/ogr2ogr -f GeoJSON data/ne_10m_admin_0_countries.geojson ${admin0}/ne_10m_admin_0_countries.shp;
+       ${gdal}/bin/ogr2ogr -f GeoJSON data/ne_10m_admin_1_states_provinces.geojson ${admin1}/ne_10m_admin_1_states_provinces.shp;
+       ${tippecanoe}/bin/tippecanoe -z3 -o data/countries-z3.mbtiles --no-progress-indicator --coalesce-densest-as-needed data/ne_10m_admin_0_countries.geojson
+       ${tippecanoe}/bin/tippecanoe -zg -Z4 -o data/states-Z4.mbtiles --no-progress-indicator --coalesce-densest-as-needed --extend-zooms-if-still-dropping data/ne_10m_admin_1_states_provinces.geojson
+       ${tippecanoe}/bin/tile-join --output-to-directory=tiles data/countries-z3.mbtiles data/states-Z4.mbtiles
     '';
+    
+   caddy-conf = pkgs.writeShellScriptBin "caddy-conf" ''
+
+   '';
+   mileto-serve = pkgs.writeShellScriptBin "mileto-serve" ''
+      caddy -conf ${caddy-conf}
+   '';
+   mileto-delete = pkgs.writeShellScriptBin "mileto-delete" ''
+      rm -rf tiles
+      rm -rf data
+   '';
 in pkgs.stdenv.mkDerivation rec {
   name = "mileto";
   src = [ admin0 admin1];
   buildInputs = [
-    piensa.tippecanoe
-    pkgs.caddy
     pkgs.bash
-    pkgs.gdal
-    admin0
-    admin1
-    build-tiles
+    pkgs.caddy
+    mileto-generate
+    mileto-serve
+    mileto-delete
   ];
 
   buildPhase = ''
