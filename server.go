c9b82516760b09854eefd809b81b7a998590e01c
Ariel <ingenieroariel@gmail.com>
2019-08-16 11:08:18 -0500

    Simpler

--- server.go
+++ server.go
@@ -2,13 +2,9 @@ package main
 
 import (
 	"github.com/caddyserver/caddy/caddy/caddymain"
-	
-	// plug in plugins here, for example:
-	// _ "import/path/here"
 )
 
 func main() {
-	// optional: disable telemetry
 	caddymain.EnableTelemetry = false
 	caddymain.Run()
 }
